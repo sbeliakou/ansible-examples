6e73fd25a089a6d78a805e801213b76a25bbf5cf
vagrant <vagrant@ansible.bm>
2017-06-22 10:00:23 +0000

    updated bash module

--- modules/library/bash-module.sh
+++ modules/library/bash-module.sh
@@ -2,6 +2,13 @@
 
 source $1
 
+##------Checking input variables---------
+
+if [ -z "$phrase" ]; then
+    printf '{"failed": true, "msg": "Missing required arguments: phrase"}'
+    exit 1
+fi
+
 printf '{"failed": false, "changed": false, "phrase": "%s"}' "$phrase"
 
 exit 0
