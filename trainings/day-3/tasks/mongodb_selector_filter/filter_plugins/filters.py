20d20945b1bae652e98211f1c9a04fecd1621239
Siarhei Beliakou <sbeliakou@gmail.com>
2018-03-14 12:24:37 +0300

    supporting ubuntu and mint

--- trainings/day-3/tasks/mongodb_selector_filter/filter_plugins/filters.py
+++ trainings/day-3/tasks/mongodb_selector_filter/filter_plugins/filters.py
@@ -11,8 +11,14 @@ def get_mongo_src(arg, os_name, os_version, mongo_version):
     import re
 
     os_name = os_name.lower()
+    if os_name == "centos":
+        os_name = "rhel"
+
     if os_name == "redhat":
         os_name = "rhel"
+
+    if os_name == "linuxmint":
+        os_name = "ubuntu"        
 
     for item in arg:
         if re.match('.*' + os_name + os_version + '.*', item, re.M|re.I) and re.match('.*' + mongo_version + '.*', item, re.M|re.I):
