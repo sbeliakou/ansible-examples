c6482301b0a771ec072adcd93cf2f0c86c7bba38
Siarhei Beliakou <sbeliakou@gmail.com>
2018-03-17 20:55:16 +0300

    updated module

--- trainings/day-3/tasks/custom_modules/library/deploy_war.py
+++ trainings/day-3/tasks/custom_modules/library/deploy_war.py
@@ -24,28 +24,31 @@ def main():
     )
   )
 
+  url = module.params["url"]
+  username = module.params["username"]
+  password = module.params["password"]
+  context = module.params["context"]
+  src = os.path.abspath(module.params["src"])
+
   result = dict(
       msg='',
       changed=False,
-      context='',
-      application_url='',
-      src=''
+      context=context,
+      application_url="{}{}".format(url, context),
+      src=src
   )
 
   if module.check_mode:
     return result
 
-  url = module.params["url"]
-  username = module.params["username"]
-  password = module.params["password"]
-  context = module.params["context"]
-  src = module.params["src"]
-
   if os.path.isfile(src) == False:
     result['msg'] = 'FAIL - File not found - {}'.format(src)
     module.fail_json(**result)
 
-  mime = subprocess.Popen("/usr/bin/file --mime " + src, shell=True, stdout=subprocess.PIPE).communicate()[0].split(' ')[1].replace(';', '')
+  mime = subprocess.Popen("/usr/bin/file --mime " + src, \
+    stdout=subprocess.PIPE, \
+    shell=True).communicate()[0].split(' ')[1].replace(';', '')
+
   if "java-archive" not in mime:
     result['msg'] = 'FAIL - Not a java-archive type - {}'.format(src)
     module.fail_json(**result)
@@ -58,9 +61,6 @@ def main():
   if r.status_code == 200:
     result['msg'] = "OK - Deployed application at context path [{}]".format(context)
     result['changed'] = True
-    result['context'] = context
-    result['application_url'] = "{}{}".format(url, context)
-    result['src'] = src
   else:
     result['msg'] = 'FAIL - Deployment Failed at context path [{}]'.format(context)
     module.fail_json(**result)
